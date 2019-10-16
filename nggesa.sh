36f35f747098415324773683a02eb49a15e96682
ozzi94 <56570719+ozzi94@users.noreply.github.com>
2019-10-17 03:53:17 +0700

    Add files via upload

---
+++ nggesa.sh
@@ -0,0 +1,11 @@
+#!/system/bin/sh
+
+echo "belajar membuat script"
+echo ""
+echo "menggunakan bahasa pemrograman"
+echo ""
+echo "semangat ya"
+sleep 3
+read oswal
+echo "hi selamat datang"$oswal
+
